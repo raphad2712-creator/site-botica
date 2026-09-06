b77ceda73daa629582ded0c3146a6bf2c6687234
Raphael David Silva Freitas <raphad2712@gmail.com>
2026-09-06 16:50:36 -0300

    Corrige imagens corrompidas dos produtos

--- lib/product-images.ts
+++ lib/product-images.ts
@@ -1,13 +1,13 @@
 import type { Produto } from "@/lib/types";
 
 const imagensCatalogo: Array<[RegExp, string]> = [
-  [/creatina monohidratada/, "/produtos/creatina-monohidratada-300g.png"],
-  [/melatonina.*gotas/, "/produtos/melatonina-gotas-20ml.png"],
-  [/floral rescue/, "/produtos/floral-rescue-30ml.png"],
-  [/vitamina b12/, "/produtos/vitamina-b12-1000mcg.png"],
-  [/vitamina d3/, "/produtos/vitamina-d3-2000ui.png"],
-  [/psyllium/, "/produtos/psyllium-200g.png"],
-  [/serum.*vitamina c/, "/produtos/serum-vitamina-c-30ml.png"],
+  [/creatina monohidratada/, "/produtos/creatina-monohidratada-300g.jpg"],
+  [/melatonina.*gotas/, "/produtos/melatonina-gotas-20ml.jpg"],
+  [/floral rescue/, "/produtos/floral-rescue-30ml.jpg"],
+  [/vitamina b12/, "/produtos/vitamina-b12-1000mcg.jpg"],
+  [/vitamina d3/, "/produtos/vitamina-d3-2000ui.jpg"],
+  [/psyllium/, "/produtos/psyllium-200g.jpg"],
+  [/serum.*vitamina c/, "/produtos/serum-vitamina-c-30ml.jpg"],
 ];
 
 function normalizar(texto: string) {
